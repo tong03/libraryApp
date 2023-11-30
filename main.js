7da83a97281b808a1306939067f8ebd33cee231e
tritong <tongtri31@gmail.com>
2023-11-30 10:53:34 -0800

    Added visual changes and create book function
    Layout and elements change for dialog modal pop up, as well as fixing
    the header layout to look more aesthetic. Implemented a working function
    to create new Book object when the submit button is clicked on the form,
    reading out the form values for each input as parameters for the Book
    constructor. Then initialized a display book function to create new
    elements on the DOM base on each Book object attribute.

--- main.js
+++ main.js
@@ -1,35 +1,51 @@
 const myLibrary = [];
 
-// constructor
-function Library() {
+const addButton = document.getElementById("addBook");
+const addDialog = document.getElementById("addDialog");
+const submitButton = document.getElementById("submitButton");
+const bookForm = document.getElementById("bookForm");
+const bookDisplay = document.getElementById("bookDisplay");
 
+// constructor
+class Book {
+    constructor(author, title, pages){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-function displayLibrary() {
-    for (book in myLibrary) {
+function displayLibrary(myLibrary) {
+    for (const book of myLibrary) {
         console.log(book);
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book");
+        bookCard.setAttribute("id", book.title);
+        const titleName = document.createElement("p");
+        titleName.innerHTML = book.title;
+        const authorName = document.createElement("p");
+        authorName.innerHTML = book.author;
+        const readBtn = document.createElement("button");
+        readBtn.innerHTML = "Read";
+        const removeBtn = document.createElement("button");
+        removeBtn.innerHTML = "Remove";
+        bookCard.appendChild(titleName);
+        bookCard.appendChild(authorName);
+        bookCard.appendChild(readBtn);
+        bookCard.appendChild(removeBtn);
+        bookDisplay.appendChild(bookCard);
     }
 }
-
-const addButton = document.getElementById("addBook");
-const addDialog = document.getElementById("addDialog");
-const submitButton = document.getElementById("submitButton");
-// const bookForm = document.getElementById("bookForm");
 
 addDialog.addEventListener("click", (event) => {
     if (event.target == addDialog) {
         addDialog.close();
     }
-    // console.log(event.target);
 });
-
-// bookForm.addEventListener("click", (event) => {
-//     event.stopPropagation();
-// });
 
 addButton.addEventListener("click", () => {
     addDialog.showModal();
@@ -37,5 +53,15 @@ addButton.addEventListener("click", () => {
 
 submitButton.addEventListener("click", (event) => {
     event.preventDefault();
+    // const formData = new FormData(bookForm);
+    const title = bookForm.title.value;
+    const author = (bookForm.author.value);
+    const pages = bookForm.pages.value;
+    const newBook = new Book(title, author, pages);
+    addBookToLibrary(newBook);
+    // for (const pair of formData.entries()){
+    //     console.log(pair[0]);
+    // }
+    displayLibrary(myLibrary);
     addDialog.close();
 });
