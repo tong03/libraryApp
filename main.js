0475ca791cebdc5695cc5ad62eecc528d7caa1ed
tritong <tongtri31@gmail.com>
2023-12-02 14:29:38 -0800

    Added functionality to remove button
    Implemented removeBtn function that removes the targeted bookCard element
    out of the DOM, update the global myLibrary list, reset index and update
    index attribute for all remaining bookCard.

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const submitButton = document.getElementById("submitButton");
 const bookForm = document.getElementById("bookForm");
 const bookDisplay = document.getElementById("bookDisplay");
 
+let i = 0;
+
 // constructor
 class Book {
     constructor(author, title, pages){
@@ -19,11 +21,20 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+function updateIndex() {
+    const bookCards = document.querySelectorAll('.book');
+    
+    bookCards.forEach((card) => {
+        card.setAttribute("index", i++);
+    });
+}
+
 function displayLibrary(myLibrary) {
     for (const book of myLibrary) {
         if (book == myLibrary[myLibrary.length - 1]){
             const bookCard = document.createElement("div");
             bookCard.classList.add("book");
+            bookCard.setAttribute("index", i++);
             bookCard.setAttribute("id", book.title);
             const titleName = document.createElement("p");
             titleName.innerHTML = book.title;
@@ -32,9 +43,21 @@ function displayLibrary(myLibrary) {
             const readBtn = document.createElement("button");
             readBtn.innerHTML = "Read";
             const removeBtn = document.createElement("button");
+            removeBtn.classList.add("rmCard");
             removeBtn.innerHTML = "Remove";
-            removeBtn.addEventListener("click", () => {
-
+            removeBtn.addEventListener("click", (e) => {
+                const parentCard = e.target.closest('.book');
+                if (parentCard){
+                    // Remove the bookCard from the global book list
+                    const rmIndex = parseInt(parentCard.getAttribute("index"), 10);
+                    myLibrary.splice(rmIndex, 1);
+                    // Reset index count to 0;
+                    i = 0;
+                    // Remove the bookCard from the DOM;
+                    parentCard.remove();
+                    // Update index of remaining bookCards in the book list
+                    updateIndex();
+                }
             });
             bookCard.appendChild(titleName);
             bookCard.appendChild(authorName);
